75896180cae98cd7e09bb1d14e41e90fda6355a5
dfagundes2001 <fagundesdiego2015bolcombr@gmail.com>
2024-07-29 19:27:57 -0300

    feat: add currency formatting function, Transaction and Card components

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,10 +1,24 @@
 import { InputMask } from '@react-input/mask';
 import { Button } from '../../components/button';
+import { ButtonIcon } from '../../components/button-icon';
+import { Card } from '../../components/card';
 import { Input } from '../../components/input';
 import { Logo } from '../../components/logo';
 import { Title } from '../../components/title';
-import { Filters, Header, InputGroup, Main, Section } from './styles';
-import { ButtonIcon } from '../../components/button-icon';
+import {
+	Aside,
+	Balance,
+	ChartAction,
+	ChartContainer,
+	ChartContent,
+	Filters,
+	Header,
+	InputGroup,
+	Main,
+	SearchTransaction,
+	Section,
+} from './styles';
+import { Transaction } from '../../components/transaction';
 
 export function Home() {
 	return (
@@ -23,24 +37,74 @@ export function Home() {
 						<InputGroup>
 							<InputMask
 								component={Input}
-								mask="dd/mm/yyyy"
-								replacement={{ d: /\d/, m: /\d/, y: /\d/ }}
+								mask="dd/mm/aaaa"
+								replacement={{ d: /\d/, m: /\d/, a: /\d/ }}
 								variant="dark"
 								label="Início"
-								placeholder="dd/mm/yyyy"
+								placeholder="dd/mm/aaaa"
 							/>
 							<InputMask
 								component={Input}
-								mask="dd/mm/yyyy"
-								replacement={{ d: /\d/, m: /\d/, y: /\d/ }}
+								mask="dd/mm/aaaa"
+								replacement={{ d: /\d/, m: /\d/, a: /\d/ }}
 								variant="dark"
 								label="Início"
-								placeholder="dd/mm/yyyy"
+								placeholder="dd/mm/aaaa"
 							/>
-							<ButtonIcon/>
+							<ButtonIcon />
 						</InputGroup>
 					</Filters>
+					<Balance>
+						<Card title="Saldo" amount={1000000} />
+						<Card title="Receitas" amount={1000000} variant="incomes" />
+						<Card title="Gastos" amount={1000000} variant="expenses" />
+					</Balance>
+					<ChartContainer>
+						<header>
+							<Title
+								title="Gastos"
+								subTitle="Despesas por categoria no período"
+							/>
+						</header>
+						<ChartContent>.</ChartContent>
+					</ChartContainer>
+					<ChartContainer>
+						<header>
+							<Title
+								title="Evolução Financeira"
+								subTitle="Saldo, Receitas e Gastos no ano"
+							/>
+							<ChartAction>
+								<InputMask
+									component={Input}
+									mask="aaaa"
+									replacement={{ d: /\d/ }}
+									variant="black"
+									label="Ano"
+									placeholder="aaaa"
+								/>
+								<ButtonIcon />
+							</ChartAction>
+						</header>
+						<ChartContent>.</ChartContent>
+					</ChartContainer>
 				</Section>
+				<Aside>
+					<header>
+						<Title title="Transações" subTitle="Receitas e Gastos no período" />
+						<SearchTransaction>
+							<Input variant="black" placeholder="Procurar transação..." />
+							<ButtonIcon />
+						</SearchTransaction>
+						<Transaction
+						id={1}
+						title="Compra de carro"
+            date="10/08/2022"
+            amount={10000}
+						category={{ title: 'Automóvel', color: '#FFA500' }}
+						/>
+					</header>
+				</Aside>
 			</Main>
 		</>
 	);
